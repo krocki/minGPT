bbc9877e4d0215e3fe919209628fc395780ec1a9
Kamil Rocki <krocki@nvidia.com>
2021-12-10 10:20:18 -0800

    jax

--- get_data.sh
+++ get_data.sh
@@ -1,2 +1,11 @@
-#!/usr/bin/bash
-wget https://github.com/karpathy/char-rnn/blob/master/data/tinyshakespeare/input.txt
+#!/bin/bash
+ADDR="https://cs.fit.edu/~mmahoney/compression"
+
+FILES=("enwik8.bz2" \
+       "enwik9.bz2")
+
+mkdir -p data && cd data
+for f in ${FILES[@]}; do
+    wget -nc ${ADDR}/$f; bunzip2 -f $f
+  done
+  cd ..
